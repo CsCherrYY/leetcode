4ef11729e50b04e4d1da7c009f90402b1fb461a6
CS <45906942+CsCherrYY@users.noreply.github.com>
2019-03-05 11:14:59 +0800

    201935

--- leetcode_vs2015/leetcode_vs2015/lcfrom227.cpp
+++ leetcode_vs2015/leetcode_vs2015/lcfrom227.cpp
@@ -238,11 +238,218 @@ public:
 		return true;
 		//O(1)空间复杂度
 	}
+	TreeNode* lowestCommonAncestor_search(TreeNode* root, TreeNode* p, TreeNode* q) {
+		//判断节点左右
+		if (p->val > q->val) {
+			return lowestCommonAncestor(root, q, p);
+		}
+		if (root->val == p->val || root->val == q->val) {
+			return root;
+		}
+		if ((root->val) > (p->val)&&(root->val) < (q->val)) {
+			return root;
+		}
+		if ((root->val) > (p->val) && (root->val) > (q->val)) {
+			return lowestCommonAncestor(root->left, p, q);
+		}
+		if ((root->val) < (p->val) && (root->val) < (q->val)) {
+			return lowestCommonAncestor(root->right, p, q);
+		}
+		return nullptr;
+	}
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == p || root == q || !root) {
+			return root;
+		}
+		TreeNode *left = lowestCommonAncestor(root->left, p, q);
+		TreeNode *right = lowestCommonAncestor(root->right, p, q);
+		if (left&&right) {
+			return root;
+		}
+		else if (left) {
+			return left;
+		}
+		else if (right) {
+			return right;
+		}
+		else {
+			return nullptr;
+		}
+	}
+	void deleteNode(ListNode* node) {
+		//请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+		ListNode *work = node->next;
+		while (1) {
+			node->val = work->val;
+			work = work->next;
+			if (!work) {
+				node->next = NULL;
+				return;
+			}
+			node = node->next;
+		}
+	}
+	vector<int> productExceptSelf(vector<int>& nums) {
+		//说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+		//进阶：常数空间复杂度
+		int size = nums.size();
+		vector<int> out(size, 1);
+		vector<int> right(size, 1);
+		//左右
+		for (int i = 1; i < size; ++i) {
+			out[i] = out[i - 1] * nums[i - 1];
+		}
+		for (int i = size - 2; i >= 0; --i) {
+			right[i] = right[i + 1] * nums[i + 1];
+		}
+		for (int i = 0; i < size; ++i) {
+			out[i] = out[i] * right[i];
+		}
+		return out;
+	}
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int size_h = matrix.size();
+		if (!size_h) {
+			return false;
+		}
+		int size_w = matrix[0].size();
+		int i = size_h - 1;
+		int j = 0;
+		while (i>=0&&j<=size_w-1) {
+			if (matrix[i][j] == target) {
+				return true;
+			}
+			else if (matrix[i][j] > target) {
+				--i;
+			}
+			else {
+				++j;
+			}
+		}
+		return false;
+	}
+	vector<int> diffWaysToCompute(string input) {
+		vector<int>res;
+		for (int i = 0; i < input.size(); ++i) {
+			char c = input[i];
+			if (c == '+' || c == '-' || c == '*') {
+				vector<int>res1 = diffWaysToCompute(input.substr(0, i));
+				vector<int>res2 = diffWaysToCompute(input.substr(i + 1));
+				for (auto s : res1) {
+					for (auto t : res2) {
+						if (c == '+') {
+							res.push_back(s + t);
+						}
+						else if (c == '-') {
+							res.push_back(s - t);
+						}
+						else if (c == '*') {
+							res.push_back(s * t);
+						}
+					}
+				}
+			}
+		}
+		if (res.empty()) {
+			res.push_back(stoi(input));
+		}
+		return res;
+	}
+	bool isAnagram(string s, string t) {
+		vector<int> vecs(26, 0);
+		vector<int> vect(26, 0);
+		for (auto c : s) {
+			++vecs[c - 'a'];
+		}
+		for (auto c : t) {
+			++vect[c - 'a'];
+		}
+		for (int i = 0; i < 26; ++i) {
+			if (vecs[i] != vect[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	vector<string> binaryTreePaths(TreeNode* root) {
+		vector<string> res;
+		string now;
+		binaryTreePaths_trace(root, res, now);
+		return res;
+	}
+	void binaryTreePaths_trace(TreeNode* root, vector<string>& res, string now) {
+		if (!root) {
+			return;
+		}
+		now += to_string(root->val);
+		if (!root->left && !root->right) {
+			res.push_back(now);
+			return;
+		}
+		now += "->";
+		binaryTreePaths_trace(root->left, res, now);
+		binaryTreePaths_trace(root->right, res, now);
+		return;
+	}
+	int addDigits(int num) {
+		//你可以不使用循环或者递归，且在 O(1) 时间复杂度内解决这个问题吗
+		//简单的实现
+		int temp = 0;
+		int i;
+		while (1) {
+			while (num) {
+				i = num % 10;
+				num /= 10;
+				temp += i;
+			}
+			if (temp < 10) {
+				return temp;
+			}
+			num = temp;
+		}
+	}
+	vector<int> singleNumber(vector<int>& nums) {
+		vector<int> res;
+		int temp = 0;
+		for (auto s : nums) {
+			temp ^= s;
+		}
+		int k = 0;
+		int last;
+		while (temp) {
+			last = temp % 2;
+			temp = temp >> 1;
+			if (!k) {
+				k = 1;
+			}
+			else {
+				k = k << 1;
+			}
+			if (last) {
+				break;
+			}
+		}
+		int a = 0;
+		int b = 0;
+		for (auto s : nums) {
+			auto l = s&k;
+			if (l == 0) {
+				a ^= s;
+			}
+			else {
+				b ^= s;
+			}
+		}
+		res.push_back(a);
+		res.push_back(b);
+		return res;
+	}
 };
 int main() {
 	Solution solu;
-	vector<int>test229;
-	int res = solu.calculate("31 + 5 / 2");
-	test229 = solu.majorityElement(vector<int>{1, 2, 2, 3, 2, 1, 1, 3});
+	vector<int>test260;
+	//int res = solu.calculate("31 + 5 / 2");
+	//test229 = solu.majorityElement(vector<int>{1, 2, 2, 3, 2, 1, 1, 3});
+	test260 = solu.singleNumber(vector<int>{1, 2, 1, 3, 2, 5});
 	return 0;
 }
---
+++ leetcode_vs2015/leetcode_vs2015/lcfrom263.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+	bool isUgly(int num) {
+		if (num < 1) {
+			return false;
+		}
+		if (num == 1) {
+			return true;
+		}
+		while (num>=1) {
+			if (num == 1) {
+				return true;
+			}
+			else if (num % 2 == 0) {
+				num /= 2;
+			}
+			else if (num % 3 == 0) {
+				num /= 3;
+			}
+			else if (num % 5 == 0) {
+				num /= 5;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+	}
+};
+int main() {
+	return 0;
+}
