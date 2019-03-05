1ff69f88fa2a9471bd9f7d24666c096047634aba
CS <45906942+CsCherrYY@users.noreply.github.com>
2019-03-05 14:39:13 +0800

    201935a

--- leetcode_vs2015/leetcode_vs2015/lcfrom263.cpp
+++ leetcode_vs2015/leetcode_vs2015/lcfrom263.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<set>
+#include<vector>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
@@ -27,6 +30,69 @@ public:
 			}
 		}
 		return false;
+	}
+	int nthUglyNumber(int n) {
+		//generate ugly
+		vector<int>uglynum(n, 1);
+		vector<int>index(3, 0);   //2,3,5
+		int a, b, c, temp;
+		for (int i = 1; i < n; ++i) {
+			a = uglynum[index[0]] * 2;
+			b = uglynum[index[1]] * 3;
+			c = uglynum[index[2]] * 5;
+			temp = min(a, min(b, c));
+			uglynum[i] = temp;
+			if (temp == a) {
+				++index[0];
+			}
+			if (temp == b) {
+				++index[1];
+			}
+			if (temp == c) {
+				++index[2];
+			}
+		}
+		return uglynum[n - 1];
+	}
+	int missingNumber(vector<int>& nums) {
+		//O(n)时间+O(n)空间，常规实现，O(1)空间解法
+		long long sum = 0;
+		for (auto s : nums) {
+			sum += s;
+		}
+		return ((nums.size() + 1)*nums.size()) / 2 - sum;
+	}
+	int hIndexI(vector<int>& citations) {
+		//o nlogn 排序+遍历
+		if (citations.size() == 0) {
+			return 0;
+		}
+		sort(citations.begin(), citations.end());
+		int h = 0;
+		for (int i = 0; i < citations.size(); ++i) {
+			int htemp = min(citations[i], int(citations.size() - i));
+			h = max(h, htemp);
+		}
+		return h;
+	}
+	int hIndex(vector<int>& citations) {
+		//条件，数组有序，二分：
+		int right = citations.size() - 1;
+		int left = 0;
+		int h = 0;
+		int mid;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (citations[mid] > (citations.size() - mid)) {
+				h = max(h, int(citations.size() - mid));
+				right = mid - 1;
+			}
+			else {
+				h = max(h, citations[mid]);
+				left = mid + 1;
+			}
+		}
+		return h;
 	}
 };
 int main() {
